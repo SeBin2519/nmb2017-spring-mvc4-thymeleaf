94f339d5fc31d655f50b7b7a6e411a2f92cca1f1
SeBinNam <nsb2519@e-mirim.hs.kr>
2017-09-20 09:43:04 +0900

    name 파라미터 추가

--- src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -1,23 +1,15 @@
 package kr.hs.emirim.nmb2017.springmvc4.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.twitter.api.SearchResults;
-import org.springframework.social.twitter.api.Tweet;
-import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller 
 public class HelloController { 
 	@RequestMapping("/")
-	 public String hello(Model model) {
-	  model.addAttribute("message", "Hello from the controller"); 
-	  return "resultPage";
-	 }
+	public String hello(@RequestParam("name") String userName, Model model) {
+		  model.addAttribute("message",userName+"¥‘, æ»≥Á«œººø‰!");
+		  return "resultPage";
+	} 
 }
